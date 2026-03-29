f9226170ee3036a72e54a0b89efa8c38d59a3048
money-3532 <money-3532@users.noreply.github.com>
2026-03-29 11:29:46 +0000

    deploy: 28ad42d8507bf416d12a3c664f13b3d31055dac1

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-const router = createRouter({
-  history: createWebHistory('/money.productivity-pro/'),
-  routes: [],
-})
-
-export default router
